7d8264763f316851aea33d347153f49478f3c464
Evan <eroberts1998@icloud.com>
2024-05-22 13:59:35 -0400

    section 5 progress

---
+++ 03-pizza-menu/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Pizza from './components/pizza'
+
+function App() {
+
+  return (
+    <>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </>
+  )
+}
+
+export default App
---
+++ 03-pizza-menu/src/components/pizza.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function pizza() {
+  return (
+    <div>
+        <h2>Focaccia</h2>
+        <p>Bread with italian olive oil and rosemary</p>
+        <img src='../../pizzas/focaccia.jpg' />
+    </div>
+  )
+}
+
+export default pizza
